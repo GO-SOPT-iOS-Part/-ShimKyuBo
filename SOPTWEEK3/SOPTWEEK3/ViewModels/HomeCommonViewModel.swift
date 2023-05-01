f4b16b7ac018942c805ebcea97b9215a91108f40
PecanPie <hidra0321@naver.com>
2023-05-01 23:56:15 +0900

    Added ViewModels to empty VC

--- SOPTWEEK3/SOPTWEEK3/ViewModels/HomeCommonViewModel.swift
+++ SOPTWEEK3/SOPTWEEK3/ViewModels/HomeCommonViewModel.swift
@@ -5,4 +5,20 @@
 //  Created by KYUBO A. SHIM on 2023/05/01.
 //
 
-import Foundation
+import UIKit
+
+import RxSwift
+import RxCocoa
+
+final class HomeCommonViewModel {
+    // 그 버튼 누르면 넘어가는거 전부 여기에 넣어
+    
+    enum HeaderTabs: String, CaseIterable {
+        case home = "홈"
+        case live = "실시간"
+        case tvSeries = "TV프로그램"
+        case movies = "영화"
+        case paramount = "파라마운트+"
+        case kids = "키즈"
+    }
+}
--- SOPTWEEK3/SOPTWEEK3/ViewModels/HomeTabViewModel.swift
+++ SOPTWEEK3/SOPTWEEK3/ViewModels/HomeTabViewModel.swift
@@ -10,56 +10,58 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-final class HomeTabViewModel: ViewModelType {
-    typealias ImageName = String
+final class HomeTabViewModel: HomeTabsType {
     
-    struct Dependency {
-        
-    }
-    
-    struct Input {
-        
-    }
-    
-    struct Output {
-        
-    }
-    
-    let dependency: Dependency
+    typealias PosterImageName = String
+    typealias SectionName = String
+
     var disposeBag: DisposeBag = DisposeBag()
-    let input: Input
-    let output: Output
+    let tabIdentity: String = HomeCommonViewModel.HeaderTabs.home.rawValue
+    private let postersArray: [PosterImageName?] = ["KiminoPoster", "SiganlPoster", "HPPoster", "LOTRPoster", "SuzumePoster"]
+    private let bannersArray: [PosterImageName?] = ["banner1", "banner2", "banner3"]
+    private let paramountArray: [PosterImageName?] = ["para1", "para2", "para3", "para4", "para5"]
+    private let tvingExclusiveArray: [PosterImageName?] = ["tvEx1", "tvEx2", "tvEx3", "tvEx4"]
     
-    enum HeaderTabs {
-        case home
-        case live
-        case tvSeries
-        case movies
-        case paramount
-        case kids
+    private let mainPosters: BehaviorRelay<[PosterImageName?]>
+    private let bestShowsPosters: BehaviorRelay<[PosterImageName?]>
+    private let topTwentyMovies: BehaviorRelay<[PosterImageName?]>
+    
+    // 화면에 나오지 않은 부분은 lazy 로 처리해서 처음 화면이 뜰 때 최대한 성능이 좋게 하려는데 이런 플로우가 맞는지 헷갈리네.. 이게 맞는 방법일까?
+    private lazy var bannerPosters: BehaviorRelay<[PosterImageName?]> = BehaviorRelay(value: [])
+    private lazy var paramountPosters: BehaviorRelay<[PosterImageName?]> = BehaviorRelay(value: [])
+    private lazy var tvingExclusivePosters: BehaviorRelay<[PosterImageName?]> = BehaviorRelay(value: [])
+    
+    enum Sections: SectionName, CaseIterable {
+        case bestShows = "티빙에서 꼭 봐야하는 콘텐츠"
+        case topMovies = "티빙 TOP 20 영화"
+        case paramountExclusive = "파라마운트+의 따끈한 신작"
+        case tvingExclusive = "오직 티빙에만 있어요"
+    }
+    
+    init() {
+        let initialMainPosters: BehaviorRelay<[PosterImageName?]> = BehaviorRelay(value: postersArray)
+        let initialBestShowsPosters: BehaviorRelay<[PosterImageName?]> = BehaviorRelay(value: postersArray.shuffled())
+        let initialTopTwentyMovies: BehaviorRelay<[PosterImageName?]> = BehaviorRelay(value: postersArray.shuffled())
         
-        var tabMenu: String {
-            switch self {
-            case .home:
-                return "홈"
-            case .live:
-                return "실시간"
-            case .tvSeries:
-                return "TV프로그램"
-            case .movies:
-                return "영화"
-            case .paramount:
-                return "파라마운트+"
-            case .kids:
-                return "키즈"
-            }
-        }
+        self.mainPosters = initialMainPosters
+        self.bestShowsPosters = initialBestShowsPosters
+        self.topTwentyMovies = initialTopTwentyMovies
+    }
+}
+
+extension HomeTabViewModel {
+    func fetchBannerPosters() -> BehaviorRelay<[PosterImageName?]> {
+        bannerPosters.accept(bannersArray)
+        return bannerPosters
     }
     
-    enum Sections {
-        case
+    func fetchParamountPosters() -> BehaviorRelay<[PosterImageName?]> {
+        paramountPosters.accept(paramountArray)
+        return paramountPosters
     }
     
-    
-    
+    func fetchTvingExclusivePosters() -> BehaviorRelay<[PosterImageName?]> {
+        tvingExclusivePosters.accept(tvingExclusiveArray)
+        return tvingExclusivePosters
+    }
 }
