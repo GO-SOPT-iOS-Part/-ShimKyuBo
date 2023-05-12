d6de84ea3669b0ecf813f6536ef09e105c0c49b5
PecanPie <hidra0321@naver.com>
2023-05-12 13:57:57 +0900

    Added Models for Network

--- SoptWeek4/SoptWeek4/Models/ChartModel.swift
+++ SoptWeek4/SoptWeek4/Models/ChartModel.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import MusicKit
+
+struct BillBoardModel: Codable {
+    typealias ChartModel = BillBoardCharts
+
+    var date: String
+    var data: [ChartModel]
+}
+
+struct BillBoardCharts: Codable {
+    let songName, artist: String
+    let albumImage: String
+    let rank: Int
+    let lastWeekRank: Int?
+    let peakRank, weeksOnChart: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case artist, rank
+        case songName = "name"
+        case albumImage = "image"
+        case lastWeekRank = "last_week_rank"
+        case peakRank = "peak_rank"
+        case weeksOnChart = "weeks_on_chart"
+    }
+}
+
+struct AppleSongsItem: Identifiable, Hashable {
+    var id = UUID()
+    let songTitle: String
+    let artistName: String
+    let imageUrl: URL?
+}
+
--- SoptWeek4/SoptWeek4/Views/Common/BaseTableViewController.swift
+++ SoptWeek4/SoptWeek4/Views/Common/BaseTableViewController.swift
@@ -7,83 +7,87 @@
 
 import UIKit
 
-class BaseTableViewController: UITableViewController {
-
+class BaseTabBarController: UITabBarController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        delegate = self
+        setSwipeGesture()
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+    private func setSwipeGesture() {
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipeGesture))
+        swipeLeft.direction = .left
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipeGesture))
+        swipeRight.direction = .right
+        
+        self.view.addGestureRecognizer(swipeLeft)
+        self.view.addGestureRecognizer(swipeRight)
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    
+    @objc func handleSwipeGesture(_ gesture: UISwipeGestureRecognizer) {
+        if gesture.direction == .left {
+            if (self.selectedIndex) < 2 { // 슬라이드할 탭바 갯수 지정 (전체 탭바 갯수 - 1)
+                animateToTab(toIndex: self.selectedIndex + 1)
+            }
+        } else if gesture.direction == .right {
+            if (self.selectedIndex) > 0 {
+                animateToTab(toIndex: self.selectedIndex - 1)
+            }
+        }
     }
+}
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+extension BaseTabBarController: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+            guard let tabViewControllers = tabBarController.viewControllers, let toIndex = tabViewControllers.firstIndex(of: viewController) else {
+                return false
+            }
+        
+            animateToTab(toIndex: toIndex)
+        
+            return true
+        }
+    
+    func animateToTab(toIndex: Int) {
+        guard let tabViewControllers = viewControllers,
+              let selectedVC = selectedViewController else { return }
+        
+        guard let fromView = selectedVC.view,
+              let toView = tabViewControllers[toIndex].view,
+              let fromIndex = tabViewControllers.firstIndex(of: selectedVC),
+              fromIndex != toIndex else { return }
+        
+        fromView.superview?.addSubview(toView)
+        
+        // Position toView off screen (to the left/right of fromView)
+        let screenWidth = UIScreen.main.bounds.size.width
+        let scrollRight = toIndex > fromIndex
+        let offset = (scrollRight ? screenWidth : -screenWidth)
+        toView.center = CGPoint(x: fromView.center.x + offset, y: toView.center.y)
+        
+        // Disable interaction during animation
+        view.isUserInteractionEnabled = false
+        
+        UIView.animate(withDuration: 0.3,
+                       delay: 0.0,
+                       usingSpringWithDamping: 1,
+                       initialSpringVelocity: 0,
+                       options: .curveEaseOut,
+                       animations: {
+            // Slide the views by -offset
+            fromView.center = CGPoint(x: fromView.center.x - offset, y: fromView.center.y)
+            toView.center = CGPoint(x: toView.center.x - offset, y: toView.center.y)
+            
+        }, completion: { finished in
+            // Remove the old view from the tabbar view.
+            fromView.removeFromSuperview()
+            
+            self.selectedIndex = toIndex
+            self.view.isUserInteractionEnabled = true
+        })
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
